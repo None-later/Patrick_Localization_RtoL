a7030584a243f2f096bc69b155dc75982dd91b04
Pratik <pratikpanchal@pratik-imac.local>
2018-01-29 21:47:01 -0800

    Initial Commit

---
+++ Patrick_Localization_RtoL/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Patrick_Localization_RtoL
+//
+//  Created by pratik panchal on 29/01/18.
+//  Copyright © 2018 pratik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
